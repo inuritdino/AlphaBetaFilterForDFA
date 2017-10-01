6a293caf38298ca5b5d91c3ce33ea53d615484a7
Ilya Potapov <elias.potapov@gmail.com>
2017-10-01 09:47:24 +0300

    Q saturation limit changeable from ab_filter()

--- ab_filter.py
+++ ab_filter.py
@@ -7,13 +7,15 @@ fluctuation analysis and ab filter.", Chaos 2003; 13(2):467-75
 
 import numpy as np
 
-def ab_filter(ls,Fs,num_ls=500):
+def ab_filter(ls,Fs,num_ls=500,Q=500):
     """
-    Usage: me,ll = ab_filter(ls,Fs,num_ls=500)
+    Usage: me,ll = ab_filter(ls,Fs,num_ls=500,Q=500)
     Inputs:
     ls - window sizes in log
     Fs - fluctuations in log
     num_ls - a number of window sizes to use in linear interpolation (default: 500).
+    Q - a saturation limit of the alpha and beta functions (default: 500). Not recommended
+        to change unless some technical tests are intended.
     Outputs:
     me - estimation of the exponents as a function of window size
     ll - uniformly sampled window sizes on a log scale (ls)
@@ -28,41 +30,39 @@ def ab_filter(ls,Fs,num_ls=500):
     ###############################
     ## Ge -- log(Fs) estimator, Gp(k) -- log(Fs) predictor based on (k-1) value
     ## me -- d(log(Fs))/d(log(ls)) estimator, i.e. gradient
-    Gp = np.zeros(len(Fs_interp))
-    Ge = np.zeros(len(Fs_interp))
-    me = np.zeros(len(Fs_interp))
+    Gp = np.zeros(num_ls)
+    Ge = np.zeros(num_ls)
+    me = np.zeros(num_ls)
     ## log(ls)-step/delta
     d = uni_ls[1] - uni_ls[0]
     ## The first estimators are the same as data
     Ge[0] = Fs_interp[0]
-    me[0] = (Fs_interp[1] - Fs_interp[0])/(uni_ls[1] - uni_ls[0])
+    me[0] = (Fs_interp[1] - Fs_interp[0])/ d
     ## The loop
-    for i in range(1,len(Fs_interp)):
+    for i in range(1,num_ls):
         Gp[i] = Ge[i-1] + d * me[i-1]
         r = Fs_interp[i] - Gp[i]
-        Ge[i] = Gp[i] + alpha(i) * r
-        me[i] = me[i-1] + (beta(i)/d) * r
+        Ge[i] = Gp[i] + alpha(i,Q) * r
+        me[i] = me[i-1] + (beta(i,Q)/d) * r
 
     return (me,uni_ls);
 
-def alpha(k):
+def alpha(k,Q):
     """Window size(k) dependent alpha value. 
     With suggestion from Echeverria et al. "Interpretation of
     heart rate variability..."
     """
-    Q = 500
     if k > Q:
-        return alpha(Q);
+        return alpha(Q,Q);
     else:
-        return 2*(2*k-1)/(k*(k+1))
+        return 2*(2*k-1)/(k*(k+1));
 
-def beta(k):
+def beta(k,Q):
     """Window size(k) dependent beta value.
     With suggestion from Echeverria et al. "Interpretation of
     heart rate variability..."
     """
-    Q = 500
     if k > Q:
-        return beta(Q);
+        return beta(Q,Q);
     else:
         return 6/(k*(k+1));
